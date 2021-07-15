0bcd0f0d98d8abe77ec84b7fdee536f78cd7a7e9
David Lapeš <dave.lapes@gmail.com>
2021-07-15 22:43:30 +0200

    Test S3 bucket deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. But this is very right now freshly deployed!
+          Edit <code>src/App.js</code> and save to reload. But this is happily freshly deployed!
         </p>
         <a
           className="App-link"
