4eda5100dfc33b53e08d4d6eb98f462079091a35
David Lapeš <dave.lapes@gmail.com>
2021-08-23 13:39:37 +0200

    Rework website into ReactJS

--- deploy-website.sh
+++ deploy-website.sh
@@ -1,3 +1,3 @@
-npm run --prefix ../frontend build
-aws s3 sync ../frontend/build/ s3://terkaberedavida.cz
+npm run build
+aws s3 sync build/ s3://terkaberedavida.cz
 aws cloudfront create-invalidation --distribution-id E73O8TVBW4WB0 --paths "/*"
