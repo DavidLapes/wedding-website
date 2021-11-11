13a7b3631d689d86a2113b95a8b94acc9fb4369c
David Lapeš <dave.lapes@gmail.com>
2021-11-11 10:14:32 +0100

    Fix bugged empty fields for RSVP

--- src/services/actions/guests/submitRSVP.js
+++ src/services/actions/guests/submitRSVP.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import {GUEST_URL, DYNAMIC_RSVP_URL} from "../../../commons/apiCommons";
 import {getExceptionResponseMessage} from "../../exception/translation";
 import {fetchGuests} from "./fetchGuests";
+import {removeEmpty} from "../../../lib/object";
 
 export const SUBMIT_RSVP_BEGIN_REQUEST = "SUBMIT_RSVP_BEGIN_REQUEST";
 export const SUBMIT_RSVP_SUCCESS = "SUBMIT_RSVP_SUCCESS";
@@ -10,7 +11,7 @@ export const SUBMIT_RSVP_ERROR = "SUBMIT_RSVP_ERROR";
 export const submitRSVP = (id, rsvp) => (dispatch, getState) => {
     if(canCreateRSVP(getState())) {
         dispatch(submitRSVPRequest());
-        return axios.post(GUEST_URL + "/" + id + DYNAMIC_RSVP_URL, {...rsvp, language: "czech"})
+        return axios.post(GUEST_URL + "/" + id + DYNAMIC_RSVP_URL, removeEmpty({...rsvp, language: "czech"}))
             .then(json => {
                 dispatch(submitRSVPSuccess(json.data));
                 dispatch(fetchGuests());
